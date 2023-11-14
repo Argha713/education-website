6afadfa81adfe302be5653c6aa6bdf8ad04710e3
Argha713 <sarkar.argha2@gmail.com>
2023-11-14 18:18:07 +0530

    WIP - Complete about page and media query for tablet

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 //change nav style on scroll
 
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
 
 //show-hide FAQ answer
 
 const faqs = document.querySelectorAll('.faq');
-faqs.forEach(faq=>{
-    faq.addEventListener('click',()=>{
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
         faq.classList.toggle('open');
 
         //change the icon
         const icon = faq.querySelector('.faq__icon i');
-        if(icon.className ==='uil uil-plus'){
+        if (icon.className === 'uil uil-plus') {
             icon.className = "uil uil-minus";
-        }else{
+        } else {
             icon.className = "uil uil-plus";
         }
     })
@@ -28,13 +28,13 @@ const menu = document.querySelector('.nav__menu');
 const menuBtn = document.querySelector('#open-menu-btn');
 const closeBtn = document.querySelector('#close-menu-btn');
 
-menuBtn.addEventListener('click', ()=>{
+menuBtn.addEventListener('click', () => {
     menu.style.display = "flex";
     closeBtn.style.display = "inline-block";
     menuBtn.style.display = "none";
 })
 
-const closeNav = ()=>{
+const closeNav = () => {
     menu.style.display = "none";
     closeBtn.style.display = "none";
     menuBtn.style.display = "inline-block";
