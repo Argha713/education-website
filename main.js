9a16c973943b76ae96bd0b47283322350e53ec3f
Argha713 <sarkar.argha2@gmail.com>
2023-11-14 15:26:59 +0530

    WIP - Tablet media query done

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.addEventListener('scroll', ()=>{
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
 
-//show/hide FAQ answer
+//show-hide FAQ answer
 
 const faqs = document.querySelectorAll('.faq');
 faqs.forEach(faq=>{
@@ -23,4 +23,21 @@ faqs.forEach(faq=>{
 
 
 
+//show-hide nav menu
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
 
+menuBtn.addEventListener('click', ()=>{
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+const closeNav = ()=>{
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav);
